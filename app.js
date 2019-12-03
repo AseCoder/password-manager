93cf8d4b2ed49eedb52bd328ca0decd44fddb44f
AseCoder <atte.elo05@gmail.com>
2019-12-03 19:57:14 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const client = require('./src/client.js');
+const moment = require('moment')
+const $ = require('jquery');
 
 client.storage.initialize();
+
+setInterval(() => {
+  const now = moment();
+  $('#hours').text(now.format('HH'));
+  $('#mins').text(now.format('mm'));
+}, 2000);
--- src/client.js
+++ src/client.js
@@ -4,6 +4,9 @@ class Client {
   constructor() {
     this.storage = new Storage();
   }
+  displayCredential(id) {
+    console.log('display credential', id);
+  }
 }
 
 module.exports = new Client();
